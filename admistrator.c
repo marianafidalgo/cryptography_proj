661e85e76c8d383b9af6df85a8a9b208cd5cb149
Mariana Fidalgo <mariana.fernandes@xpand-it.com>
2019-11-14 12:44:35 +0000

    m

--- admistrator.c
+++ admistrator.c
@@ -1,12 +1,6 @@
-<<<<<<< Updated upstream
 //testcc
 //test já no lab
 
 //sou o xico oioi
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
-=======
 
 //olá sou a matilde
->>>>>>> 11ead2a2a18c756911feac309d70326be3ebbd39
